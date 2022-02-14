9e95a338c99e16b56601e07de4bad709ac16fc87
Matthew Jakeman <mjakeman26@outlook.co.nz>
2022-02-15 00:43:29 +1300

    Update licence header

--- extension.js
+++ extension.js
@@ -1,8 +1,11 @@
 /* extension.js
  *
+ * Copyright 2022 Matthew Jakeman <mjakeman26@outlook.co.nz>
+ * Copyright 2022 System76/Pop Shell contributors
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
@@ -13,10 +16,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * SPDX-License-Identifier: GPL-2.0-or-later
+ * SPDX-License-Identifier: GPL-3.0-or-later
  */
-
-/* exported init */
 
 const GETTEXT_DOMAIN = 's76-scheduler-plugin';
 
