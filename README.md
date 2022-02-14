3f43a85da2390096c8d0140f7167b8187d959a3c
Matthew Jakeman <mjakeman26@outlook.co.nz>
2022-02-15 00:40:57 +1300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# System76 Scheduler - GNOME Shell Plugin
+This is a standalone extension that integrates `system76-scheduler` without
+needing `pop-shell` installed.
+
+You should also install `system76-scheduler` separately. I maintain a COPR
+for Fedora and derivatives [here](https://copr.fedorainfracloud.org/coprs/mjakeman/system76-scheduler/).
+
+The majority of code in this plugin comes from `pop-shell` and is used under
+the `GPL-3.0` licence.
---
+++ extension.js
@@ -0,0 +1,85 @@
+/* extension.js
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+/* exported init */
+
+const GETTEXT_DOMAIN = 's76-scheduler-plugin';
+
+const { GObject, St } = imports.gi;
+
+const ExtensionUtils = imports.misc.extensionUtils;
+const Main = imports.ui.main;
+
+const Gio = imports.gi.Gio;
+
+const SchedulerInterface = '<node>\
+<interface name="com.system76.Scheduler"> \
+    <method name="SetForegroundProcess"> \
+        <arg name="pid" type="u" direction="in"/> \
+    </method> \
+</interface> \
+</node>';
+
+const SchedulerProxy = Gio.DBusProxy.makeProxyWrapper(SchedulerInterface)
+
+const SchedProxy = new SchedulerProxy(
+    Gio.DBus.system,
+    "com.system76.Scheduler",
+    "/com/system76/Scheduler"
+)
+
+let foreground = 0
+
+class Extension {
+    constructor(uuid) {
+        this._uuid = uuid;
+
+        ExtensionUtils.initTranslations(GETTEXT_DOMAIN);
+    }
+
+    enable() {        
+        log("Initialising system76-scheduler integration");
+        
+        this._handler = global.display.connect('notify::focus-window', () => {
+            let meta_window = global.display.focus_window;
+                        
+            const pid = meta_window.get_pid()
+            
+            if (pid) {
+                if (foreground === pid) return
+                foreground = pid
+
+                try {
+                    log(`Setting priority for ${meta_window.get_title()}`)
+                    SchedProxy.SetForegroundProcessRemote(pid)
+                } catch (_) {}
+            }
+        });
+    }
+
+    disable() {
+        this._indicator.destroy();
+        this._indicator = null;
+
+        global.display.disconnect(this._handler);
+    }
+}
+
+function init(meta) {
+    return new Extension(meta.uuid);
+}
